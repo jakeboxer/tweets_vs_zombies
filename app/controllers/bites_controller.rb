61ef1bf82abecc87059f246fc99c292d19201452
Jake Boxer <jake@github.com>
2014-08-16 17:10:42 -0700

    Show twitter signin on bites#create

--- app/controllers/bites_controller.rb
+++ app/controllers/bites_controller.rb
@@ -1,38 +1,11 @@
 class BitesController < ApplicationController
   def create
-    user = User.find_by(:username, username)
+    user = User.find_by(:username, sanitized_username)
 
-    if user.nil?
-      # Grab user from twitter
-      user = User.create(:username => username) if twitter_user.present?
-    end
-
-    if user.present?
-      if user.needs_twitter_check?
-        # TODO(jakeboxer): Make sure this updates updated_at even when the
-        # last_tweeted_at timestamp doesn't change
-        user.last_tweeted_at = twitter_user.tweets.first.created_at
-        user.save
-      end
-
-      if user.zombie?
-        # "@user is already a zombie!"
-      elsif logged_in?
-        if twitter_user.followed_by?(current_user)
-          if twitter_user.biteable?
-            # Bite
-          else
-            # "@user has tweeted recently, wait until they fall asleep!"
-          end
-        else
-          # "You can only bite people who follow you!""
-        end
-      else
-        # Log in with twitter
-      end
-    else
-      # "@user isn't a real user on twitter!"
-    end
+    render "bites/sign_in", :locals => {
+      :user     => user,
+      :username => sanitized_username
+    }
   end
 
   def new
